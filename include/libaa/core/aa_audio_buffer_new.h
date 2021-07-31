2b11e33ed2f24c3c0dc0c3ea0d5c38cad97d8254
huawei.william <huawei.william@bytedance.com>
2021-08-01 00:17:41 +0800

    fix linux build error

--- include/libaa/core/aa_audio_buffer_new.h
+++ include/libaa/core/aa_audio_buffer_new.h
@@ -15,17 +15,13 @@ template <typename T>
 class AudioBufferNew
 {
 public:
+    explicit AudioBufferNew(const std::list<std::vector<T>>& channel_data) = delete;
+
     explicit AudioBufferNew(size_t num_channels, size_t num_frames):
         num_channels_(num_channels),
         num_frames_(num_frames)
     {
         allocateData();
-    }
-
-    explicit AudioBufferNew(const std::list<std::vector<T>>& channel_data):
-        num_channels_(channel_data.size())
-    {
-        copyFromVectors(channel_data);
     }
 
     explicit AudioBufferNew(std::list<std::vector<T>>&& channel_data):
