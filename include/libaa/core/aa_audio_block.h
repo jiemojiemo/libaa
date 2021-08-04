8f178af7fafa0e90e2c7adfd45b396f598def11f
huawei.william <huawei.william@bytedance.com>
2021-08-04 08:22:52 +0800

    add audio block

---
+++ include/libaa/core/aa_audio_block.h
@@ -0,0 +1,29 @@
+
+//
+// Created by William.Hua on 2021/8/4.
+//
+
+#ifndef LIBAA_INCLUDE_LIBAA_CORE_AA_AUDIO_BLOCK_H
+#define LIBAA_INCLUDE_LIBAA_CORE_AA_AUDIO_BLOCK_H
+#pragma once
+#include "libaa/core/aa_audio_buffer_new.h"
+#include "libaa/core/aa_parameter_changes.h"
+namespace libaa
+{
+class AudioBlock {
+public:
+    explicit AudioBlock();
+
+    explicit AudioBlock(int num_channels, int num_frames);
+
+    explicit AudioBlock(std::list<std::vector<float>>&& init_list);
+
+    explicit AudioBlock(AudioBufferNew<float> buffer, ParameterChanges changes);
+
+    AudioBufferNew<float> audio_buffer;
+    ParameterChanges param_changes;
+};
+
+}
+
+#endif //LIBAA_INCLUDE_LIBAA_CORE_AA_AUDIO_BLOCK_H
---
+++ src/core/aa_audio_block.cpp
@@ -0,0 +1,35 @@
+
+//
+// Created by William.Hua on 2021/8/4.
+//
+
+#include "libaa/core/aa_audio_block.h"
+
+namespace libaa
+{
+AudioBlock::AudioBlock():
+    AudioBlock(0, 0)
+{
+
+}
+
+AudioBlock::AudioBlock(int num_channels, int num_frames):
+    audio_buffer(num_channels, num_frames)
+{
+
+}
+
+
+AudioBlock::AudioBlock(std::list<std::vector<float>>&& init_list):
+audio_buffer(std::move(init_list))
+{
+
+}
+
+AudioBlock::AudioBlock(AudioBufferNew<float> buffer, ParameterChanges changes):
+audio_buffer(std::move(buffer)),
+param_changes(std::move(changes))
+{
+
+}
+}
