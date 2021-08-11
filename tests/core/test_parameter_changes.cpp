8417484774b62b5c60a4ce2d3d40562fac5e0271
william <460706836@qq.com>
2021-08-11 22:57:11 +0800

    update parameter changes, that can insert parameter change

--- tests/core/test_parameter_changes.cpp
+++ tests/core/test_parameter_changes.cpp
@@ -24,7 +24,14 @@ TEST_F(AParameterChanges, DefautConstructInitWithEmptyParameterRingBuffer)
 {
     auto param_changes = ParameterChanges{};
 
-    ASSERT_TRUE(param_changes.getParameterChangesArray().empty());
+    ASSERT_TRUE(param_changes.getParameterChangeSet().empty());
+}
+
+TEST_F(AParameterChanges, IsEmptyWhenConstructByDefault)
+{
+    auto param_changes = ParameterChanges{};
+
+    ASSERT_TRUE(param_changes.empty());
 }
 
 TEST_F(AParameterChanges, CanConstuctWithSizeOfParamterChange)
@@ -32,7 +39,7 @@ TEST_F(AParameterChanges, CanConstuctWithSizeOfParamterChange)
     size_t num_param_change_buffer = 2;
     auto param_changes = ParameterChanges{num_param_change_buffer};
 
-    ASSERT_THAT(param_changes.getParameterChangesArray().size(), Eq(num_param_change_buffer));
+    ASSERT_THAT(param_changes.getParameterChangeSet().size(), Eq(num_param_change_buffer));
 }
 
 TEST_F(AParameterChanges, ConstructWithParameterChangePoints)
@@ -52,7 +59,7 @@ TEST_F(AParameterChanges, ConstructWithPointsHasSizeOfDifferentIndex)
         {1, 0, 0.2}
     };
 
-    ASSERT_THAT(param_changes.getParameterChangesArray().size(), Eq(2));
+    ASSERT_THAT(param_changes.getParameterChangeSet().size(), Eq(2));
 }
 
 TEST_F(AParameterChanges, ConstructWithPointsWillInsertPointIntoRingbuffer)
@@ -80,8 +87,31 @@ TEST_F(AParameterChanges, RingbufferHasIncreasedIndex)
     size_t num_param_change_buffer = 2;
     auto param_changes = ParameterChanges{num_param_change_buffer};
 
-    const ParameterChanges::RingBuffers& ringbuffers = param_changes.getParameterChangesArray();
+    const ParameterChanges::ParameterChangeSet& ringbuffers = param_changes.getParameterChangeSet();
 
-    ASSERT_THAT(ringbuffers[0]->getParameterIndex(), Eq(0));
-    ASSERT_THAT(ringbuffers[1]->getParameterIndex(), Eq(1));
+    ASSERT_THAT(ringbuffers.at(0)->getParameterIndex(), Eq(0));
+    ASSERT_THAT(ringbuffers.at(1)->getParameterIndex(), Eq(1));
+}
+
+TEST_F(AParameterChanges, IsNotEmptyIfThereHasParameterChangePoint)
+{
+    auto param_changes = ParameterChanges{
+        {0, 0, 0.1},
+        {0, 0, 0.1},
+        {1, 0, 0.2},
+        {1, 0, 0.2}
+    };
+
+    ASSERT_FALSE(param_changes.empty());
+}
+
+TEST_F(AParameterChanges, InsertIncreaseTheNumberOfParameterChangePoint)
+{
+    auto param_changes = ParameterChanges{};
+
+    param_changes.insert({0, 0, 0.5});
+    ASSERT_THAT(param_changes.getNumChanges(), Eq(1));
+
+    param_changes.insert({0, 0, 0.5});
+    ASSERT_THAT(param_changes.getNumChanges(), Eq(2));
 }
