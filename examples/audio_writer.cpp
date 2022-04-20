90056d0698c295f395c67928eb787f7a2699f1ba
jiemojiemo <460706836@qq.com>
2022-04-20 11:38:07 +0800

    add temp example

--- examples/audio_writer.cpp
+++ examples/audio_writer.cpp
@@ -2,48 +2,55 @@
 //
 // Created by William.Hua on 2021/3/9.
 //
-#include "libaa/fileio/aa_wav_audio_format_writer.h"
-#include "libaa/fileio/aa_wav_audio_format_reader.h"
 #include "libaa/fileio/aa_file_input_stream.h"
 #include "libaa/fileio/aa_file_output_stream.h"
-#include <memory>
-#include <string>
+#include "libaa/fileio/aa_wav_audio_format_reader.h"
+#include "libaa/fileio/aa_wav_audio_format_writer.h"
 #include <fstream>
 #include <iostream>
+#include <memory>
+#include <string>
 #include <vector>
 using namespace std;
 using namespace libaa;
-int main()
-{
-    const string input_filename = "../../res/sound/es01_2ch_48k_i16.wav";
+int main() {
+    const string input_filename = "/Users/user/Downloads/test_test_re.wav";
     string output_filename = "wav_writer_out.wav";
 
     auto in_file = std::make_unique<FileInputStream>(input_filename);
-    if(!in_file->isOpen()){
+    if (!in_file->isOpen()) {
         cerr << "cannot open input file\n";
         return -1;
     }
 
     WaveAudioFormatReader reader(std::move(in_file));
-    const int sample_rate = reader.sample_rate;
-    const int num_channels = reader.num_channels;
+    const int sample_rate = 44100;
+    const int num_channels = 1;
     const int num_frames = reader.length_in_samples;
     cout << "sample rate: " << sample_rate << endl;
     cout << "number of channels: " << num_channels << endl;
     cout << "number of total frames: " << num_frames << endl;
     vector<float> buffer(num_channels * num_frames);
-    vector<float*> data_refer_to(num_channels);
-    for(auto c = 0; c < num_channels; ++c){ data_refer_to[c] = buffer.data() + c*num_frames; }
+    vector<float *> data_refer_to(num_channels);
+    for (auto c = 0; c < num_channels; ++c) {
+        data_refer_to[c] = buffer.data() + c * num_frames;
+    }
 
     // read whole file
-    reader.readSamples(data_refer_to.data(), num_channels, 0, 0,num_frames);
+    reader.readSamples(data_refer_to.data(), num_channels, 0, 0, num_frames);
 
+    output_filename = "impulse_0.wav";
     auto out_file = std::make_unique<FileOutputStream>(output_filename);
-    if(!out_file->isOpen()){
+    if (!out_file->isOpen()) {
         cerr << "cannot open output file\n";
         return -1;
     }
-    WavFormatWriter writer(std::move(out_file), sample_rate, num_channels, 32);
-    writer.writePlanar((const float**)(data_refer_to.data()), num_frames);
+    //    sample_rate = 44100;
+
+    WavFormatWriter writer(std::move(out_file), sample_rate, 1, 32);
+    vector<float> l(1024, 0);
+    l[0] = 1;
+    data_refer_to[0] = l.data();
+    writer.writePlanar((const float **)(data_refer_to.data()), 1024);
     writer.close();
 }
--- examples/load_file.cpp
+++ examples/load_file.cpp
@@ -2,34 +2,40 @@
 // Created by william on 2019/12/1.
 //
 #include "libaa/fileio/aa_audio_file.h"
-#include <string>
 #include <iostream>
+#include <string>
 using namespace std;
 using namespace libaa;
 
-int main(int argc, char* argv[])
-{
-    if(argc < 2)
-    {
-        cerr << "Usage: load_file {audio-filename} [{output-filename}]" << endl;
-        return -1;
-    }
-
-    string input_filename = argv[1];
-    string output_filename = "load_file_result.wav";
-    if(argc == 3)
-    {
-        output_filename = argv[2];
-    }
+int main(int argc, char *argv[]) {
+    //    if(argc < 2)
+    //    {
+    //        cerr << "Usage: load_file {audio-filename} [{output-filename}]" <<
+    //        endl; return -1;
+    //    }
+    //
+    //    string input_filename = argv[1];
+    //    string output_filename = "load_file_result.wav";
+    //    if(argc == 3)
+    //    {
+    //        output_filename = argv[2];
+    //    }
 
     AudioFile audio_file;
-    int failed = audio_file.load(input_filename);
-    if(failed)
-    {
-        cerr << "load file failed\n";
-        return 0;
-    }
-
+    audio_file.load("/Users/user/Downloads/test_test_re.wav");
+    
+    audio_file.setSampleRate(44100);
+    audio_file.setNumBits(32);
+    audio_file.setNumChannles(1);
+    audio_file.setNumFrames(1024);
+    audio_file.samples[0][0] = 1.0f;
+    //    int failed = audio_file.load(input_filename);
+    //    if(failed)
+    //    {
+    //        cerr << "load file failed\n";
+    //        return 0;
+    //    }
+    auto output_filename = "impulse_0.wav";
     audio_file.saveToWave(output_filename);
 
     return 0;
