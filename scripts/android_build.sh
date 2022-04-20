133129b17e88d43dbf64831808c1e8cdeb60367c
jiemojiemo <460706836@qq.com>
2022-04-20 19:28:56 +0800

    add android_build job

--- scripts/android_build.sh
+++ scripts/android_build.sh
@@ -1,17 +1,17 @@
 # android armv7
-cmake -S . -B android_build \
+cmake -S . -B build_android_armv7 \
   -DCMAKE_TOOLCHAIN_FILE="$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake" \
   -DANDROID_ABI="armeabi-v7a" -DANDROID_ARM_NEON=ON -DANDROID_PLATFORM=android-19 \
   -DBUILD_TESTING=OFF \
   -DAE_BUILD_REGRESSION_DEMO=OFF
 
-cmake --build android_build --verbose -- -j 8
+cmake --build build_android_armv7 -- -j 8
 
-## android aarch64
-#cmake -S . -B android_build \
-#  -DCMAKE_TOOLCHAIN_FILE="$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake" \
-#  -DANDROID_ABI="arm64-v8a" -DANDROID_ARM_NEON=ON -DANDROID_PLATFORM=android-19 \
-#  -DAE_BUILD_TESTING=OFF \
-#  -DAE_BUILD_REGRESSION_DEMO=OFF
-#
-#cmake --build android_build -- -j 8
+# android aarch64
+cmake -S . -B build_android_aarch64 \
+  -DCMAKE_TOOLCHAIN_FILE="$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake" \
+  -DANDROID_ABI="arm64-v8a" -DANDROID_ARM_NEON=ON -DANDROID_PLATFORM=android-19 \
+  -DBUILD_TESTING=OFF \
+  -DAE_BUILD_REGRESSION_DEMO=OFF
+
+cmake --build build_android_aarch64 -- -j 8
