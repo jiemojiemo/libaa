2723853483b6576de2bb94b4a905ad429533bbf4
jiemojiemo <460706836@qq.com>
2022-05-08 16:28:26 +0800

    add source processor

---
+++ include/libaa/processor/aa_source_processor.h
@@ -0,0 +1,33 @@
+//
+// Created by user on 5/8/22.
+//
+
+#ifndef LIBAA_AA_SOURCE_PROCESSOR_H
+#define LIBAA_AA_SOURCE_PROCESSOR_H
+
+#pragma once
+#include "libaa/processor/aa_audio_processor.h"
+#include <cassert>
+namespace libaa {
+class SourceProcessor : public IAudioProcessor {
+public:
+    SourceProcessor();
+
+    SourceProcessor(std::list<std::vector<float>> channel_data);
+
+    ~SourceProcessor() override = default;
+
+    std::string getName() const override;
+
+    void processBlock(AudioBlock *input, AudioBlock *output) override;
+
+    const AudioProcessorParameters *getParameters() const override;
+
+    const AudioBufferX<float> *getChannelData() const;
+
+private:
+    std::unique_ptr<AudioBufferX<float>> channel_data_{nullptr};
+};
+} // namespace libaa
+
+#endif // LIBAA_AA_SOURCE_PROCESSOR_H
---
+++ tests/processor/test_source_processor.cpp
@@ -0,0 +1,49 @@
+//
+// Created by user on 5/8/22.
+//
+#include "libaa/processor/aa_source_processor.h"
+
+#include <gmock/gmock.h>
+
+using namespace testing;
+using namespace libaa;
+class ASourceProcessor : public Test {
+public:
+    float sample_rate = 44100;
+    float max_block_size = 10;
+};
+
+TEST_F(ASourceProcessor, CanReportCorrectName) {
+    SourceProcessor proc;
+
+    ASSERT_THAT(proc.getName(), Eq("Source Processor"));
+}
+
+TEST_F(ASourceProcessor, HasNoParameters) {
+    SourceProcessor proc;
+
+    ASSERT_THAT(proc.getParameters(), IsNull());
+}
+
+TEST_F(ASourceProcessor, CanInitWithChannelData) {
+    SourceProcessor proc{
+        {{1, 2, 3},
+         {4, 5, 6}}};
+
+    ASSERT_THAT(proc.getChannelData()->getNumberChannels(), Eq(2));
+    ASSERT_THAT(proc.getChannelData()->getNumberFrames(), Eq(3));
+}
+
+TEST_F(ASourceProcessor, ProcessWillCopyChannelDataToOutput) {
+    SourceProcessor proc{
+        {{1, 2, 3},
+         {4, 5, 6}}};
+
+    AudioBlock block{2, 3, 0};
+
+    proc.prepareToPlay(sample_rate, max_block_size);
+    proc.processBlock(&block, &block);
+
+    ASSERT_THAT(block.buffer.getReadPointer(0)[0], Eq(1));
+    ASSERT_THAT(block.buffer.getReadPointer(1)[0], Eq(4));
+}
