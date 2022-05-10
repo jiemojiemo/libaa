d475bba06381c77dab1a1707ee2bf17dafc3b3b6
jiemojiemo <460706836@qq.com>
2022-05-10 10:32:33 +0800

    vibrato processor can set/get state

--- tests/processor/test_vibrator_processor.cpp
+++ tests/processor/test_vibrator_processor.cpp
@@ -2,8 +2,10 @@
 // Created by William on 3/25/22.
 //
 #include "aa_testing_helper.h"
+#include "libaa/processor/aa_processor_utilities.h"
 #include "libaa/processor/aa_vibrator_processor.h"
 #include <gmock/gmock.h>
+#include <nlohmann/json.hpp>
 using namespace testing;
 using namespace libaa;
 
@@ -56,4 +58,25 @@ TEST_F(AVibratorProcessor, ProcessAsExpect) {
     AudioBlock expected{{{0, 0.922973632, 1, 1}, {0, 0.922973632, 1, 1}}};
 
     ASSERT_THAT(block.buffer, Eq(expected.buffer));
+}
+
+TEST_F(AVibratorProcessor, StateStringAsExpected) {
+    auto state = proc.getState();
+
+    auto expected_string = ProcessorUtilities::serializeProcessorToString(&proc);
+
+    ASSERT_THAT(ProcessorUtilities::convertProcessorStateToString(state), Eq(expected_string));
+}
+
+TEST_F(AVibratorProcessor, SetStateUpdatesParameters) {
+    float expected_param_val = 1.0;
+    auto state_str = ProcessorUtilities::serializeProcessorToString(&proc);
+    auto state_json = nlohmann::json::parse(state_str);
+    state_json["parameters"]["Depth"] = expected_param_val;
+    auto new_state_str = nlohmann::to_string(state_json);
+
+    proc.setState((uint8_t *)new_state_str.data(), new_state_str.size());
+
+    float gain_val = proc.getParameters()->get(1).getPlainValue();
+    ASSERT_THAT(gain_val, Eq(expected_param_val));
 }
